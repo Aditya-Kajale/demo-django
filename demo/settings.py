0231d1b827741d0e36dc30b732d2fd299b88637b
root <root@ip-172-31-26-59.ec2.internal>
2023-02-22 17:17:03 +0000

    from ubuntu

--- demo/settings.py
+++ demo/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['34.229.124.104']
 
 # Application definition
 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_cassandra_engine',
+    'app1',
 ]
 
 MIDDLEWARE = [
